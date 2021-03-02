522265cbc193379fe0ba6d5abc9106451a6835d9
Santiago <santiago.mooser@pm.me>
2021-03-02 11:48:45 +0800

    feat (django): Added homepage view and template

--- src/covid_interface/views.py
+++ src/covid_interface/views.py
@@ -1,7 +1,11 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 
 def homepage(request):
-    return HttpResponse("YEEEET")
+
+    template = loader.get_template('views/homepage.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
