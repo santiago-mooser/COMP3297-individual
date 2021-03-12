d36a78b23fc5cf453728b61dc19dcd479ac234be
Santiago <santiago.mooser@pm.me>
2021-03-12 18:50:19 +0800

    fix(django): updated forms.py due to invalid attribute in form

--- src/covid_interface/forms.py
+++ src/covid_interface/forms.py
@@ -5,7 +5,7 @@ class homePageForm(forms.Form):
     location = forms.CharField(max_length=100)
 
 class newResourceForm(forms.Form):
-    location_name   = forms.CharField(max_length=150, unique=True)
+    location_name   = forms.CharField(max_length=150)
     est_population  = forms.IntegerField()
     api_endpoint    = forms.URLField()
     resource_url    = forms.URLField()
