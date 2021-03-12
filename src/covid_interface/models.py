2748b06bcdf8ef788314e6aff6cb41b798099a2b
Santiago <santiago.mooser@pm.me>
2021-03-12 13:32:39 +0800

    feat(django): Added models

--- src/covid_interface/models.py
+++ src/covid_interface/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Country(models.Model):
+    location_name   = models.CharField(max_length=150, unique=True)
+    est_population  = models.BigIntegerField()
+    api_endpoint    = models.URLField()
+    resource_url    = models.URLField()
