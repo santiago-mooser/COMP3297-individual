5c48efbd0135c47bcae73aa720195fffef73220f
Santiago <santiago.mooser@pm.me>
2021-03-25 22:05:23 +0800

    egg(django): I'm a teapot!

--- src/covid_interface/urls.py
+++ src/covid_interface/urls.py
@@ -3,10 +3,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #Redirect default views to HK
     path('', views.proxy, name='proxy'),
     path('country/', views.proxy, name='proxy'),
+
+    #Path to add a new resource
+    path('add/', views.newResource, name='new_resource'),
+
+    # Once those resources have been added, you can see,
+    # delete or update them, or anjoy a nice cup of tea.
     path('country/<str:loc_name>', views.homepage, name='homepage'),
     path('delete/<str:loc_name>', views.delete, name='delete'),
     path('update/<str:loc_name>', views.update, name='update'),
-    path('add/', views.newResource, name='new_resource'),
+    path('418/', views.teapot, name="teapot")
 ]
